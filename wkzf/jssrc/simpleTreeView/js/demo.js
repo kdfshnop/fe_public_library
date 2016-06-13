a25c9b91d2f768efee2a1815ba20436b47b12494
Kingsley <yuxiaochen@guanaihui.com>
2016-06-13 17:51:18 +0800

    add SimpleTreeView

---
+++ wkzf/jssrc/simpleTreeView/js/demo.js
@@ -0,0 +1,20 @@
+$(function(){
+	$("#tree").simpleTreeView({
+		sourceUrl: 'http://dev01.fe.wkzf/fe_public_library/bootstrap/plugins/bootstrap-treeview/src/js/bootstrap-treeview.js',
+		apiUrl: 'http://dev01.fe.wkzf/fe_public_library/wkzf/jssrc/simpleTreeView/data/city.json',
+		dataType:'json',
+		stateSynch:false,
+		bootstrapTreeParams:{
+			color:'#4081d6'
+		}
+	});
+
+	$("#tree").on('nodeChecked',function(e,node){
+		console.log(node);
+	});
+
+	$('#btnSave').on('click',function(){
+		var checkedNodes=$('#tree').simpleTreeView('getChecked');
+		console.log(checkedNodes);
+	});
+});
