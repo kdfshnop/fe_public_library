c5f591d0cb54103620c425beed26843452c48ba0
Kingsley <yuxiaochen@guanaihui.com>
2016-06-23 18:59:01 +0800

    update for TreeViewSelect disabled case

--- wkzf/jssrc/treeViewSelect/js/demo.js
+++ wkzf/jssrc/treeViewSelect/js/demo.js
@@ -1,14 +1,16 @@
 	$(function() {
 	    $('#area').treeViewSelect({
-	        sourceUrl: 'http://dev01.fe.wkzf/fe_public_library/wkzf/jssrc/treeViewSelect/lib/bootstrap-treeview.js',
+	        sourceUrl: 'http://dev01.fe.wkzf/fe_public_library/bootstrap/plugins/bootstrap-treeview/src/js/bootstrap-treeview.js',
 	        apiUrl: 'http://dev01.fe.wkzf/fe_public_library/wkzf/jssrc/treeViewSelect/data/city.json',
 	        dataType: 'json',
-	        multiSelect:true
+	        bootstrapTreeParams: {
+	            multiSelect: true
+	        }
 	    });
 
 	    $('#btnTest').on('click', function() {
 	        $('#area').treeViewSelect({
-	            sourceUrl: 'http://dev01.fe.wkzf/fe_public_library/wkzf/jssrc/treeViewSelect/lib/bootstrap-treeview.js',
+	            sourceUrl: 'http://dev01.fe.wkzf/fe_public_library/bootstrap/plugins/bootstrap-treeview/src/js/bootstrap-treeview.js',
 	            apiUrl: 'http://dev01.fe.wkzf/fe_public_library/wkzf/jssrc/treeViewSelect/data/city.json',
 	            dataType: 'json'
 	        });
