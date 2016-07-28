10b771fe01791a9412a53865364176c343358395
Kingsley <yuxiaochen@guanaihui.com>
2016-07-28 13:02:00 +0800

    TreeViewSelect add cascadeText setting

--- wkzf/jssrc/treeViewSelect/js/demo.js
+++ wkzf/jssrc/treeViewSelect/js/demo.js
@@ -3,6 +3,7 @@
 	        sourceUrl: 'http://dev01.fe.wkzf/fe_public_library/bootstrap/plugins/bootstrap-treeview/src/js/bootstrap-treeview.js',
 	        apiUrl: 'http://dev01.fe.wkzf/fe_public_library/wkzf/jssrc/simpleTreeView/data/city.json',
 	        dataType: 'json',
+	        cascadeText:true,
 	        bootstrapTreeParams: {
 	            multiSelect: false
 	        }
