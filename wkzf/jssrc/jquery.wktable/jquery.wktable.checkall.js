4c9dea2e059d4d6369fc61ab55a99a0255db7182
tangxuyang <tangxuyang@lifang.com>
2016-10-25 14:10:17 +0800

    jquery.wktable.checkall.js

--- wkzf/jssrc/jquery.wktable/jquery.wktable.checkall.js
+++ wkzf/jssrc/jquery.wktable/jquery.wktable.checkall.js
@@ -67,4 +67,18 @@
             }
         });
     };
+
+    $.fn.getCheckedItems = function(){
+        var wktable = $(this).data('wk.table');
+        if(wktable){
+            var trs = [];
+            wktable.$table.find('tbody .icon-18:not(.icon-18-unchecked)').closest('tr').each(function(index,ele){
+                trs.push(ele);
+            });
+
+            return trs;
+        }
+
+        return null;
+    }
 }(jQuery);
